d531015e84e07e4d7be737f114d9284cc33b5b92
yzzlqyxc <yzzlqyxc@163.com>
2023-01-12 00:18:22 +0800

    init

--- xv6_rust/src/main.rs
+++ xv6_rust/src/main.rs
@@ -1,3 +1,9 @@
 #![no_std]
 #![no_main]
 mod lang_items;
+
+use core::arch::global_asm;
+use core::include_str;
+
+global_asm!(include_str!("kernel/entry.S"));
+global_asm!(include_str!("kernel/kernelvec.S"));
