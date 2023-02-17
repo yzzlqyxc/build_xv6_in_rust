b0c0ba72537c5433615a5a5fb6bf2af85a8dd602
yzzlqyxc <yzzlqyxc@163.com>
2023-02-17 09:53:23 +0800

    pc error

--- xv6_rust/src/kernel/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod memlayout;
-pub mod param;
-pub mod riscv;
-
-use memlayout::*;
-use param::*;
-use riscv::*;
--- xv6_rust/src/kernel/riscv.rs
+++
@@ -1,2 +0,0 @@
-pub const PGSIZE : usize = 4096;
-pub const MAXVA : usize = 1 << (9 + 9 + 9 + 12 - 1);
